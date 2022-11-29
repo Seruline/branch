d36c630fbb259ed417a760f1959280fb560ccfd1
Alina <lina-pyankova@mail.ru>
2022-11-30 02:07:12 +0800

    add

--- app.js
+++ app.js
@@ -1,1 +1,15 @@
 alert('Hi')
+const slides = document.querySelectorAll('.slide');
+
+for (const slide of slides){
+    slide.addEventListener('click', () =>{
+        clearActiveClass()
+        slide.classList.add('active')
+    })
+}
+
+function clearActiveClass(){
+    for (const slide of slides){
+            slide.classList.remove('active')
+    }
+}
