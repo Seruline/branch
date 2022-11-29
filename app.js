87e606d36dcd0cead91aab2b59b8c899d7d838e7
Alina <lina-pyankova@mail.ru>
2022-11-30 01:21:50 +0800

    add files to main

---
+++ app.js
@@ -0,0 +1,14 @@
+const slides = document.querySelectorAll('.slide');
+
+for (const slide of slides){
+    slide.addEventListener('click', () =>{
+        clearActiveClass()
+        slide.classList.add('active')
+    })
+}
+
+function clearActiveClass(){
+    for (const slide of slides){
+            slide.classList.remove('active')
+    }
+}
